4140f9fa520ec29db70ceb2fd621f9488c90fb1a
muhammad-hamza-shahid <50265613+muhammad-hamza-shahid@users.noreply.github.com>
2020-10-05 19:14:38 +0500

    intital commit

---
+++ android/app/src/main/kotlin/com/hamza/quiz_taker_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.hamza.quiz_taker_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,180 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(Quizzler());
+
+class Quizzler extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.grey.shade900,
+        body: SafeArea(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 10.0),
+            child: QuizPage(),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class QuizPage extends StatefulWidget {
+  @override
+  _QuizPageState createState() => _QuizPageState();
+}
+
+class _QuizPageState extends State<QuizPage> {
+  List<Icon> scoreKeeper = [];
+
+  List<String> questions = [
+    'Some cats are actually allergic to humans',
+    'You can lead a cow down stairs but not up stairs.',
+    'Approximately one quarter of human bones are in the feet.',
+    'A slug\'s blood is green.',
+    'Buzz Aldrin\'s mother\'s maiden name was \"Moon\".',
+    'It is illegal to pee in the Ocean in Portugal.',
+    'No piece of square dry paper can be folded in half more than 7 times.',
+  ];
+
+  List<bool> answers = [true, false, true, true, true, true, false];
+  int q = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: <Widget>[
+        Expanded(
+          flex: 5,
+          child: Padding(
+            padding: EdgeInsets.all(10.0),
+            child: Center(
+              child: Text(
+                questions[q],
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 25.0,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: EdgeInsets.all(15.0),
+            child: FlatButton(
+              textColor: Colors.white,
+              color: Colors.green,
+              child: Text(
+                'True',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20.0,
+                ),
+              ),
+              onPressed: () {
+                if (answers[q] == true) {
+                  print('user picked right');
+                  setState(() {
+                    scoreKeeper.add(
+                      Icon(
+                        Icons.check,
+                        color: Colors.green,
+                      ),
+                    );
+                    q++;
+                  });
+                } else {
+                  print('user picked wrong');
+                  setState(() {
+                    scoreKeeper.add(
+                      Icon(
+                        Icons.close,
+                        color: Colors.red,
+                      ),
+                    );
+                    q++;
+                  });
+                }
+                // setState(() {
+                //   scoreKeeper.add(
+                //     Icon(
+                //       Icons.check,
+                //       color: Colors.green,
+                //     ),
+                //   );
+                //   q++;
+                // });
+                //The user picked true.
+              },
+            ),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: EdgeInsets.all(15.0),
+            child: FlatButton(
+              color: Colors.red,
+              child: Text(
+                'False',
+                style: TextStyle(
+                  fontSize: 20.0,
+                  color: Colors.white,
+                ),
+              ),
+              onPressed: () {
+                //The user picked false.
+                if (answers[q] == false) {
+                  print('user picked right');
+                  setState(() {
+                    scoreKeeper.add(
+                      Icon(
+                        Icons.check,
+                        color: Colors.green,
+                      ),
+                    );
+                    q++;
+                  });
+                } else {
+                  print('user picked wrong');
+                  setState(() {
+                    scoreKeeper.add(
+                      Icon(
+                        Icons.close,
+                        color: Colors.red,
+                      ),
+                    );
+                    q++;
+                  });
+                }
+                // setState(() {
+                //   scoreKeeper.add(
+                //     Icon(
+                //       Icons.close,
+                //       color: Colors.red,
+                //     ),
+                //   );
+                //   q++;
+                // });
+              },
+            ),
+          ),
+        ),
+        SafeArea(
+          child: Row(
+            children: scoreKeeper,
+          ),
+        )
+      ],
+    );
+  }
+}
+
+/*
+question1: 'You can lead a cow down stairs but not up stairs.', false,
+question2: 'Approximately one quarter of human bones are in the feet.', true,
+question3: 'A slug\'s blood is green.', true,
+*/
